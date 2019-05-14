02726b03d231e353fb3d2e2ba5a6cd628ab11fe6
Marcus Hoffmann <bubu@bubu1.eu>
2019-05-14 20:34:06 +0200

    make flask shell work

--- app.py
+++ app.py
@@ -6,18 +6,19 @@ from orm import db, User
 from argon2 import PasswordHasher
 from sqlalchemy.orm.exc import NoResultFound
 
-app = connexion.App(__name__)
+connexion_app = connexion.App(__name__)
+app = connexion_app.app
 #app.add_api('openapi.yml', resolver=RestyResolver('api'), strict_validation=True, validate_responses=True)
-app.add_api('openapi.yml', resolver=RestyResolver('api'), strict_validation=True)
+connexion_app.add_api('openapi.yml', resolver=RestyResolver('api'), strict_validation=True)
 # due to using connexion the flask app is under app.app
-app.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #app.app.config['SQLALCHEMY_ECHO'] = True
 
 if __name__ == '__main__':
-    db.init_app(app.app)
-    app.app.app_context().push()
+    db.init_app(app)
+    app.app_context().push()
     db.create_all()
     try:
         User.query.filter_by(name="admin").one()
@@ -26,4 +27,4 @@ if __name__ == '__main__':
         user = User(name="admin", password=ph.hash("admin"))
         db.session.add(user)
         db.session.commit()
-    app.run(debug=True, threaded=True)
+    connexion_app.run(debug=True, threaded=True)
