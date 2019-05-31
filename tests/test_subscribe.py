a82cd660bc7b4c609292e0851419a33da996d3bc
Marcus Hoffmann <bubu@bubu1.eu>
2019-05-31 20:13:27 +0200

    tests: add more subscription tests

--- tests/test_subscribe.py
+++ tests/test_subscribe.py
@@ -4,6 +4,7 @@ import pycurl
 import sseclient
 from io import BytesIO
 import time
+import pytest
 
 from orm import Message
 
@@ -11,26 +12,75 @@ from orm import Message
 # Want to test for:
 # * No messages are ever lost
 # * messages go to the correct client
-# * client only connected once
 
 
 class TestSubscribe:
-    def test_receive_stored(self, testserver, db):
+    def test_receive(self, testserver, testapp, db):
         url = testserver.url + "/subscribe"
         c = CurlClient(url, "aaaaAAAAbbbbBBBB0000111-C1")
-        c.get(timeout=0.1)
+        # receiving stored messages
+        c.get()
         client = sseclient.SSEClient(c.buffer)
         m1 = json.loads(next(client.events()).data)
         m2 = json.loads(next(client.events()).data)
-        client.close()
         assert m1["body"] == "Body1"
         assert m2["body"] == "Body2"
         assert len(db.session.query(Message).all()) == 0
+
+        # receiving new messages
+        testapp.post_json(
+            "/message",
+            {"body": "Message1"},
+            headers={"X-Openpush-Key": "aaaaAAAAbbbbBBBB0000111-A1"},
+        )
+        c.get()
+        assert json.loads(next(client.events()).data)["body"] == "Message1"
+        assert len(db.session.query(Message).all()) == 0
+        # messages are stored after client disconnect
+        client.close()
         c.shutdown()
+        testapp.post_json(
+            "/message",
+            {"body": "Message1"},
+            headers={"X-Openpush-Key": "aaaaAAAAbbbbBBBB0000111-A1"},
+        )
+        assert len(db.session.query(Message).all()) == 1
+
+    def test_multiple_connect(self, testserver):
+        """Test that if the same client connects again the first instance is
+        disconnected """
+        url = testserver.url + "/subscribe"
+        multi = pycurl.CurlMulti()
+        c = CurlClient(url, "aaaaAAAAbbbbBBBB0000111-C1", multi)
+        c.get()
+        c2 = CurlClient(url, "aaaaAAAAbbbbBBBB0000111-C1", multi)
+        c2.get()
+        assert c.is_finished()
+
+    def test_different_clients(self, testserver, testapp):
+        """Test connection by different clients"""
+        testapp.post_json(
+            "/message",
+            {"body": "Message1"},
+            headers={"X-Openpush-Key": "aaaaAAAAbbbbBBBB0000111-A1"},
+        )
+        url = testserver.url + "/subscribe"
+        multi = pycurl.CurlMulti()
+        c = CurlClient(url, "aaaaAAAAbbbbBBBB0000111-C1", multi)
+        c2 = CurlClient(url, "aaaaAAAAbbbbBBBB0000111-C2", multi)
+        c.get()
+        assert not c.is_finished()
+        client = sseclient.SSEClient(c.buffer)
+        # client1 should have a message
+        next(client.events())
+        client2 = sseclient.SSEClient(c2.buffer)
+        # and client2 shouldn't
+        with pytest.raises(StopIteration):
+            next(client2.events())
 
 
 class CurlClient:
-    def __init__(self, url, access_key):
+    def __init__(self, url, access_key, multi=None):
         self.buffer = BytesIO()
         c = pycurl.Curl()
         c.setopt(c.URL, url)
@@ -40,11 +90,17 @@ class CurlClient:
         c.setopt(c.WRITEDATA, self.buffer)
         self.easy = c
 
-        self.multi = pycurl.CurlMulti()
+        if multi:
+            self.multi = multi
+        else:
+            self.multi = pycurl.CurlMulti()
         self.multi.add_handle(c)
 
-    def get(self, timeout=1.0):
+    def get(self, timeout=0.1):
+        """This calls get on the associated multi-handle. So it processes all
+        transfers in parallel """
         start = time.time()
+        readpopinter = self.buffer.tell()
         while True:
             ret, num_handles = self.multi.perform()
             if ret != pycurl.E_CALL_MULTI_PERFORM:
@@ -60,7 +116,13 @@ class CurlClient:
                 ret, num_handles = self.multi.perform()
                 if ret != pycurl.E_CALL_MULTI_PERFORM:
                     break
-        self.buffer.seek(0)
+        self.buffer.seek(readpopinter)
+
+    def get_finished(self):
+        return self.multi.info_read()[1]
+
+    def is_finished(self):
+        return self.easy in self.get_finished()
 
     def shutdown(self):
         self.multi.close()
